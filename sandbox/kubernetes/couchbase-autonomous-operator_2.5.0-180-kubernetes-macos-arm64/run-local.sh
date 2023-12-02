cf71e0307b8aba572fea2beb45c3d442edf553a7
Alexander Turevskiy <3716384+oltur@users.noreply.github.com>
2023-12-02 23:12:56 +0100

    ignore

--- sandbox/kubernetes/couchbase-autonomous-operator_2.5.0-180-kubernetes-macos-arm64/run-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl apply -f crd.yaml  
-bin/cao create admission   
-bin/cao create operator
-kubectl apply -f couchbase-cluster.yaml
-kubectl get pods
-# wait for pods to be ready
-kubectl port-forward cb-example-0000 8091
